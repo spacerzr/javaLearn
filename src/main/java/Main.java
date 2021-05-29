aa245fc6bae30d5acb0d7cf5cd165086b7156fb3
Dmitry Zorin <Zorin.D.Ge@sberbank.ru>
2021-05-29 16:26:29 +0300

    добавил комменты

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,10 +3,13 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) throws Exception {
 
+        // создали объект для обработки
         CsvHandler handler = new CsvHandler();
 
+        //создали заголовок
         String[] header = new String[]{"One", "Two", "Three", "Four"};
 
+        //создали массив для данных и заполнили его случайными числами
         int[][] data = new int[10][];
         Random random = new Random();
 
@@ -17,9 +20,12 @@ public class Main {
             }
         }
 
+        //создали объект AppData
         AppData toWrite = new AppData(header, data);
 
+        //путь куда будем записывать
         String path = "C:\\Users\\space\\Desktop\\javaLearn\\my-file.csv";
+        //записываем сформированный объект в файл. Если успешно записали - читаем его же и печатаем в консоль
         if(handler.write(toWrite, path)) {
             System.out.println("Файл успешно записан");
             AppData result = handler.read(path);
