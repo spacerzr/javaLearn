5ebae9de90bd3749df6afe19faab204ba947a7b7
Dmitry Zorin <Zorin.D.Ge@sberbank.ru>
2021-05-29 16:24:07 +0300

    Поменял местами порядок действий

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,15 +5,6 @@ public class Main {
 
         CsvHandler handler = new CsvHandler();
 
-        AppData result = handler.read("C:\\Users\\space\\Desktop\\javaLearn\\my.csv");
-
-        if(result != null) {
-            System.out.println("Прочитан CSV файл:");
-            System.out.println(result);
-        } else {
-            System.out.println("Файла нет. Путь указан неверно.");
-        }
-
         String[] header = new String[]{"One", "Two", "Three", "Four"};
 
         int[][] data = new int[10][];
@@ -28,12 +19,19 @@ public class Main {
 
         AppData toWrite = new AppData(header, data);
 
-
-        if(handler.write(toWrite, "C:\\Users\\space\\Desktop\\javaLearn\\second.csv")) {
+        String path = "C:\\Users\\space\\Desktop\\javaLearn\\my-file.csv";
+        if(handler.write(toWrite, path)) {
             System.out.println("Файл успешно записан");
+            AppData result = handler.read(path);
+
+            if(result != null) {
+                System.out.println("Прочитан CSV файл:");
+                System.out.println(result);
+            } else {
+                System.out.println("Файла нет. Путь указан неверно.");
+            }
         } else {
             System.out.println("Файл не записан");
         }
-
     }
 }
